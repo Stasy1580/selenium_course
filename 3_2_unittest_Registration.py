52e6e789c59348d91c7402cd82a0bc9c48d5f1a4
Stasy <stasy_sitaram@inbox.ru>
2024-12-24 14:20:35 +0800

    Improvment of code

--- 3_2_unittest_Registration.py
+++ 3_2_unittest_Registration.py
@@ -7,12 +7,9 @@ link1 = "http://suninjuly.github.io/registration1.html"
 link2 = "http://suninjuly.github.io/registration2.html"
 
 class TestAbs(unittest.TestCase):
-    #def fill_regisration_form (link):
-
-
-    def test_registration1(self):
+    def fill_registration_form (self, link):
         self.browser = webdriver.Chrome()
-        self.browser.get(link1)
+        self.browser.get(link)
 
         input1 = self.browser.find_element(By.CSS_SELECTOR, "input:required.first")
         input1.send_keys("Ivan")
@@ -32,27 +29,12 @@ class TestAbs(unittest.TestCase):
 
         self.browser.quit()
 
+    def test_registration1(self):
+        self.fill_registration_form (link1)
+
     def test_registration2(self):
-        self.browser = webdriver.Chrome()
-        self.browser.get(link2)
+        self.fill_registration_form(link2)
 
-        input1 = self.browser.find_element(By.CSS_SELECTOR, "input:required.first")
-        input1.send_keys("Maria")
-        input2 = self.browser.find_element(By.CSS_SELECTOR, "input:required.second")
-        input2.send_keys("Petrova")
-        input3 = self.browser.find_element(By.CLASS_NAME, "third")
-        input3.send_keys("aaa@mail.ru")
-        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
-        button.click()
-
-        time.sleep(1)
-
-        welcome_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
-        welcome_text = welcome_text_elt.text
-
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Element not found")
-
-        self.browser.quit()
 
 if __name__ == "__main__":
     unittest.main()
